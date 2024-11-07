6328b72694f32b8538337d97504c2e88f8c892fb
Joseph Chow <sortofsleepy@gmail.com>
2024-11-06 16:47:54 -0800

    Work out initial sizing
    - work out initial sizing
    - since there's no explicit mobile requirement, may leave for now and come back to this if there's time.
    - git is reporting un-versioned files for some reason(maybe cause this is the first push on a new branch); this should also update that as well.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import {buildCityNode} from "./city.ts";
 // get ref to navbar
 const nav = document.querySelector("#cities")
 
+// get ref to highlighter
+const highlighter = document.querySelector("#selector")
+
 // build nodes for each city
 data.cities.forEach(city => {
     nav!.innerHTML += buildCityNode(city)
+})
+
+window.addEventListener("resize", () => {
+
 })
